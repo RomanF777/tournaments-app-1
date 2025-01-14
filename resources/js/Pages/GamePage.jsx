072a255c0b7d44f90753ed2b10573eab2aec17b9
Roman <roman.a.frolov@gmail.com>
2025-01-14 14:51:32 +0200

    Rendering Page V2

--- resources/js/Pages/GamePage.jsx
+++ resources/js/Pages/GamePage.jsx
@@ -3,14 +3,11 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 // import { usePage } from '@inertiajs/react';
 import Welcome from './Welcome';
 
-const GamePage = ({ tournament }) => {
+const GamePage = () => {
     return (
       <div style={{color: 'black'}}>
         <AuthenticatedLayout>
-          <div>
-            <h1>Game Page</h1>
-            <h2>{tournament.name}</h2>
-          </div>
+
         </AuthenticatedLayout>
         </div>
     );
